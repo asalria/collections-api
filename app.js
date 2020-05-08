d5f446431f807f5236a029b4215c35631b9b6ea2
Amelia Salgado <6043258+asalria@users.noreply.github.com>
2020-05-08 14:39:59 +0400

    fix: CORS

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ const collectionRoutes = require('./routes/collection.routes');
 app.use(cors({origin: 'https://ameliacollections-web.herokuapp.com'}));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'POST, OPTIONS,GET,DELETE,PUT');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
