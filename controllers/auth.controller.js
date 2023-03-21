a8d426ea0560112611b41a35d7520b563bfbdc36
Amelia Salgado <6043258+asalria@users.noreply.github.com>
2023-03-21 21:19:24 +0400

    fix: expressJwt

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const expressJwt = require('express-jwt');
+const { expressjwt: jwt } = require("express-jwt");
 const User = require('../models/user.model');
 const _ = require('lodash');
 const { OAuth2Client } = require('google-auth-library');
